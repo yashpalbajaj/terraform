3e631f8fec7029af5c7a82df305dccc4eb09c1ef
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2023-06-15 08:34:52 +0530

    Update vars.tf

--- azure_webserver/vars.tf
+++ azure_webserver/vars.tf
@@ -1,8 +1,3 @@
-variable "prefix" {
-  description = "The prefix which should be used for all resources in this example"
-  default = "webserver"
-}
-
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
   default = "East US"
