e11ffa8d63d1175d6f96601b8eb394b58efbd528
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2023-06-30 15:51:20 +0530

    Update apache2.sh

--- azure_webserver/apache2.sh
+++ azure_webserver/apache2.sh
@@ -3,7 +3,7 @@
 sudo apt-get update && sudo apt-get -y install apache2
 
 
-echo '<!doctype html><html><body><h1>Hello if you see this than you have apache running!</h1></body></html>' | sudo tee /var/www/html/index.html
+echo '<!doctype html><html><body><h1>Hello if you see this than you have apache running. RUN 2!</h1></body></html>' | sudo tee /var/www/html/index.html
 
 sudo systemctl restart apache2
 sudo systemctl enable apache2
