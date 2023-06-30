7b657434cfc5fbf6159b7e2ceb9b45491c99c9fa
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2023-06-30 15:50:39 +0530

    Update main.tf

--- azure_webserver/main.tf
+++ azure_webserver/main.tf
@@ -77,7 +77,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   name                            = "${trimspace(data.template_file.prefix.rendered)}-vm"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
-  size                            = "Standard_F1"
+  size                            = "Standard_F2"
   admin_username                  = "adminuser"
   admin_password                  = "P@ssw0rd1234!"
   disable_password_authentication = false
