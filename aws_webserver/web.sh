8728c83ee834c5d3d70c3d9f60761a9a0bf67849
yashpalbajaj <60441314+yashpalbajaj@users.noreply.github.com>
2023-08-04 16:39:45 +0530

    Update web.sh

--- aws_webserver/web.sh
+++ aws_webserver/web.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo apt-get update -y
+sudo apt update && apt upgrade
 sudo apt-get install apache2 -y
 sudo apt-get install wget
+sudo apt install wget
+sleep 60
 sudo cd /var/www/html/
 sudo wget https://www.free-css.com/assets/files/free-css-templates/download/page275/roxy.zip
 sudo unzip roxy.zip
