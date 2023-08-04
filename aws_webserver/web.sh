31243b091deefab39cc53889b23d181c261f5563
yashpalbajaj <60441314+yashpalbajaj@users.noreply.github.com>
2023-08-04 16:09:30 +0530

    Update web.sh

--- aws_webserver/web.sh
+++ aws_webserver/web.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install apache2 -y
+sudo cd /var/www/html/
+sudo wget https://www.free-css.com/assets/files/free-css-templates/download/page275/roxy.zip
+sudo unzip roxy.zip
+sudo cp -rvf roxy/* .
+sudo rm -rf roxy roxy.zip
 sudo systemctl restart apache2
 sudo systemctl enable apache2
